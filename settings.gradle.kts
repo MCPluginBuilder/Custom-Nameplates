51052046265f3090bfa976f493a1e3e951ef265a
星凌 <92240364+XingLingQAQ@users.noreply.github.com>
2025-11-27 22:49:20 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,15 @@ pluginManagement {
         google()
     }
 }
+resolutionStrategy {
+        eachPlugin {
+            // 强制将 com.gradleup.shadow 映射到具体的 jar 包
+            if (requested.id.id == "com.gradleup.shadow") {
+                useModule("com.gradleup.shadow:shadow-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
 rootProject.name = "CustomNameplates"
 include(":api")
 include(":backend")
